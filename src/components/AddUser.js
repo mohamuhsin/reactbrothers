abd4c6bd7fdcc82a1834212b02a8c0dc463f84bd
mohamuhsin <muhsinohoho@outlook.com>
2023-08-11 16:43:12 +0300

    done with creating card compoenent and styling it

--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -1,17 +1,20 @@
 import React from "react";
+import Card from "./Card";
 
 const AddUser = () => {
   const addUserHandler = (event) => {
     event.preventDefault();
   };
   return (
-    <form onSubmit={addUserHandler}>
-      <label htmlFor="username">UserName</label>
-      <input id="username" type="text" />
-      <label htmlFor="age">Age</label>
-      <input id="age" type="number" />
-      <button input="submit">Add Bro</button>
-    </form>
+    <Card>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">UserName</label>
+        <input id="username" type="text" />
+        <label htmlFor="age">Age</label>
+        <input id="age" type="number" />
+        <button input="submit">Add Bro</button>
+      </form>
+    </Card>
   );
 };
 export default AddUser;
