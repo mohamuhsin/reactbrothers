34a433be135cf12099e41b3c36a3e464b461d306
mohamuhsin <muhsinohoho@outlook.com>
2023-08-11 17:09:22 +0300

    got started with fetchinh users

--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -1,19 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "./Card";
 import classes from "./AddUser.module.css";
 import Button from "./Button";
 
 const AddUser = () => {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+
   const addUserHandler = (event) => {
     event.preventDefault();
+  };
+
+  const nameHandler = () => {
+    setName();
+  };
+  const ageHandler = () => {
+    setAge();
   };
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">UserName</label>
-        <input id="username" type="text" />
-        <label htmlFor="age">Age</label>
-        <input id="age" type="number" />
+        <input id="username" type="text" value={name} />
+        <label htmlFor="age">Age (Years)</label>
+        <input id="age" type="number" value={age} />
         <Button input="submit">Add Bro</Button>
       </form>
     </Card>
