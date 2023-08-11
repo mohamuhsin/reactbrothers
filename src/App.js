ed13fc2208d32e3a87fcffce9cf3f93456e482c3
mohamuhsin <muhsinohoho@outlook.com>
2023-08-11 20:58:34 +0300

    introduced fragments

--- src/App.js
+++ src/App.js
@@ -14,10 +14,10 @@ function App() {
     });
   };
   return (
-    <div>
+    <React.Fragment>
       <AddUser onAddUser={AddUserHandler} />
       <UserList users={usersList} />
-    </div>
+    </React.Fragment>
   );
 }
 
